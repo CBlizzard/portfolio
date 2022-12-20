3e8530fc570163908e954ce044c9c5e41b55fd5d
ushijima-kakashi <jaimewar07@gmail.com>
2022-12-20 13:38:16 +0530

    use import for images

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 minimal portfolio site
 
-[tutorial link](https://youtu.be/b0pkpcD8Ms4)
+[tutorial link](https://youtu.be/b0pkpcD8Ms4)  
 [site link](https://ushijima-kakashi.github.io/portfolio/)
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,25 +1,30 @@
+import broadJump from "/assets/broad_jump.jpg"
+import ultraInstinct from "/assets/ultra_instinct.jpg"
+import rolling_thunder from "/assets/rolling_thunder.jpg"
+import jajaken from "/assets/jajaken.jpg"
+
 export default [
     {
         title: 'broad jump spike',
-        imgUrl: './assets/broad_jump.jpg',
+        imgUrl: broadJump,
         stack: ['HTML', 'CSS', 'webpack'],
         link: 'https://www.buddymagazine.org/issue-6/mystery-behind-hinatas-jump-1-ptbzb',
     },
     {
         title: 'ultra instinct',
-        imgUrl: './assets/ultra_instinct.jpg',
+        imgUrl: ultraInstinct,
         stack: ['grit', 'resilience', 'practice'],
         link: 'https://dragonballuniverse.fandom.com/wiki/Ultra_Instinct',
     },
     {
         title: 'rolling thunder again',
-        imgUrl: './assets/rolling_thunder.jpg',
+        imgUrl: rolling_thunder,
         stack: ['nishinoya', 'libero', 'instinct'],
         link: 'https://www.youtube.com/watch?v=AB1-AKvodok',
     },
     {
         title: 'jajanken',
-        imgUrl: './assets/jajaken.jpg',
+        imgUrl: jajaken,
         stack: ['hatsu','nen'],
         link: 'https://characterprofile.fandom.com/wiki/Gon_Freecss',
     },
