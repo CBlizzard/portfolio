1d41601e1ade30b8454da8fd107beeb2429ed041
ushijima-kakashi <92268573+ushijima-kakashi@users.noreply.github.com>
2022-12-20 12:48:24 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,25 +1,25 @@
 export default [
     {
         title: 'broad jump spike',
-        imgUrl: '/assets/broad_jump.jpg',
+        imgUrl: './assets/broad_jump.jpg',
         stack: ['HTML', 'CSS', 'webpack'],
         link: 'https://www.buddymagazine.org/issue-6/mystery-behind-hinatas-jump-1-ptbzb',
     },
     {
         title: 'ultra instinct',
-        imgUrl: '/assets/ultra_instinct.jpg',
+        imgUrl: './assets/ultra_instinct.jpg',
         stack: ['grit', 'resilience', 'practice'],
         link: 'https://dragonballuniverse.fandom.com/wiki/Ultra_Instinct',
     },
     {
         title: 'rolling thunder again',
-        imgUrl: '/assets/rolling_thunder.jpg',
+        imgUrl: './assets/rolling_thunder.jpg',
         stack: ['nishinoya', 'libero', 'instinct'],
         link: 'https://www.youtube.com/watch?v=AB1-AKvodok',
     },
     {
         title: 'jajanken',
-        imgUrl: '/assets/jajaken.jpg',
+        imgUrl: './assets/jajaken.jpg',
         stack: ['hatsu','nen'],
         link: 'https://characterprofile.fandom.com/wiki/Gon_Freecss',
     },
